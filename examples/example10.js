5984107f73ab7fde54603321ab77bb5c62362687
Priyanshu Tanwar <148524382+Priyanshutanwar19@users.noreply.github.com>
2026-01-21 15:57:05 +0530

    till ternary

---
+++ examples/example10.js
@@ -0,0 +1,3 @@
+//ternary operator
+let x=10;
+x===10 ? console.log("The value of x is 10") : console.log("not 10");
---
+++ examples/example7.js
@@ -0,0 +1,4 @@
+let x;
+let y= x ??=10;//nullish operator(x ki koi value hai ya nhi ye check ho rha hai agar nhi hai koi value toh ten hojegi wrnha jo hogi wahi hojegi)
+console.log(y);
+
